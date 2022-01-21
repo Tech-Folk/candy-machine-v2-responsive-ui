9687eb0c9af95942cf9fafa2119422107f7e15c0
TyrellBroughton <86334360+TyrellBroughton@users.noreply.github.com>
2022-01-20 17:39:28 -0800

    updates

--- src/Components/MintFoot/MntFoot.js
+++ src/Components/MintFoot/MntFoot.js
@@ -6,8 +6,19 @@ import Twitter from "./footer_icons/Twittericon.svg"
 function MntFoot() {
   return (
        <ftbar>
-          <a href="https://twitter.com/techfolk_nft" target="_blank" rel="noopener noreferrer" style={{color: '#4A90E2', textDecoration: 'none' }}><img src={Twitter} alt = "Twitter Logo" /> Twitter</a>
-          <a href="https://discord.gg/Ut72g67DNS" target="_blank" rel="noopener noreferrer" style={{color: '#444EF2' , textDecoration: 'none', marginLeft: 150 }}><img src={Discord} alt = "Discord Icon" /> Discord</a>
+
+          <a href="https://twitter.com/techfolk_nft" target="_blank" rel="noopener noreferrer" style={{textDecoration: 'none' }}>
+            <center>
+              <img src={Twitter} alt = "Twitter Logo" />
+              <text style={{color: '#4A90E2', textDecoration: 'none' }}> Twitter</text>
+            </center>
+          </a>
+          <a href="https://discord.gg/Ut72g67DNS" target="_blank" rel="noopener noreferrer" style={{textDecoration: 'none', marginLeft: 150 }}>
+            <center>
+              <img src={Discord} alt = "Discord Logo" />
+              <text style={{color: '#444EF2', textDecoration: 'none' }}> Discord</text>
+            </center>
+          </a>
        </ftbar>
   )
 }
